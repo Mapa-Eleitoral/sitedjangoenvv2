d218ed5fb5852a8a62285bbf6c0ba40fde1fcee1
diasfilipe <filipedias@msn.com>
2025-06-02 14:23:08 -0300

    redeploy

--- siteDjangoProject/siteDjango/settings.py
+++ siteDjangoProject/siteDjango/settings.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
